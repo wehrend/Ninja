54b086d6864ce2f67d73bde3dc4178784d745c1d
Sven Wehrend <sven@BPN.TU-BERLIN.DE>
2016-07-21 14:50:16 +0200

    Use VRTK and Sword in Main Scene, removed Standard assets

--- Assets/Scripts/Lightsabber.cs
+++ Assets/Scripts/Lightsabber.cs
@@ -4,43 +4,15 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody))]
 public class Lightsabber : MonoBehaviour {
 
-    public Rigidbody lightsabber;
-    private Camera mainCamera;
-
-	// Use this for initialization
-	void Start() {
-        mainCamera = Camera.main;
-        lightsabber = GetComponent<Rigidbody>();
-        lightsabber.rotation = Quaternion.Euler(45f, 0f, 45f);
-        lightsabber.position = mainCamera.ScreenToWorldPoint(new Vector3(0, 0, 0));
-	}
-
-    // Update is called once per frame
-    void FixedUpdate()
+    void OnCollisionEnter(Collision collision)
     {
-
-        Vector3 screen = Input.mousePosition;
-        screen.z = 1.0f;
-        Vector3 position = mainCamera.ScreenToWorldPoint(screen);
-        position.y = 1.0f;
-        lightsabber.MovePosition(position);
-        //Debug.Log("Lightsabber Position" + lightsabber.position);
+        Debug.Log("Collision");
+        //device.TriggerHapticPulse(500);
+        // Debug-draw all contact points and normals
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            Debug.DrawRay(contact.point, contact.normal, Color.white);
+        }
     }
 
- /*   void OnDrawGizmos() {
-
-        Vector3 screen = Input.mousePosition;
-        screen.z = 10.0f;
-	    Vector3 position= mainCamera.ScreenToWorldPoint(screen);
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawSphere(position, 0.1F);
-        //Debug.Log("Lightsabber Position" + lightsabber.position);
-    }*/
-
-   void OnMouseDown()
-    {
-        //lightsabber.position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
-        //Debug.Log("Lightsabber Position" + lightsabber.position);
-    }
-    
 }
---
+++ Assets/Scripts/TestTrigger.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Valve.VR;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class TestTrigger : MonoBehaviour {
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        	var deviceIndex = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Leftmost);
+        	if (deviceIndex != -1 && SteamVR_Controller.Input(deviceIndex).GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+        		SteamVR_Controller.Input(deviceIndex).TriggerHapticPulse(1000);
+        
+    }
+}

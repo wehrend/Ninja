610c8ce581b336ff96998d16abd916c5709bee17
svenwehrend <sven.m.wehrend@campus.tu-berlin.de>
2016-07-31 20:13:52 +0200

    implemented angle and not repeating random

--- Assets/Scripts/FruitCanon.cs
+++ Assets/Scripts/FruitCanon.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
+using System;
+using Random= UnityEngine.Random;
 using System.Collections;
+using System.Collections.Generic;
 
 public class FruitCanon : MonoBehaviour {
 
     public float pausetime=5;
+    public int angle = 90;
+    public int maxInstances=3;
     public Transform target;
     public Fruit[] fruits;
+    private Fruit[] activeFruits;
     //public Vector3 startPosition = new Vector3(-0.5f,0.5f,-0.5f);
     //public Vector3 endPosition = new Vector3(-0.1f,2f,-0.1f);
 
@@ -37,9 +43,24 @@ public class FruitCanon : MonoBehaviour {
         //choose randomly from fruit prefabs and instantiate canon
         Fruit prefab = fruits[Random.Range(0, fruits.Length)];
         prefab.target = target;
-        Vector3 position = new Vector3(Random.Range(-10f, 10f), 2f, Random.Range(8f, 10f));
+        List<float> used_x = new List<float>();
+        float x_max = 1.5f*Mathf.Sin(Mathf.Deg2Rad* (angle / 2)) + 1.5f; //1.5 offset of the vive-cube
+        float x = Random.Range(-x_max, x_max);
+        while(used_x.Contains(x))
+        {
+            x = Random.Range(-x_max, x_max);
+            used_x.Add(x);
+        }
+
+        float z = Random.Range(8f, 10f);
+        Vector3 position = new Vector3(x, 2f, z);
+
         // wait some small time
         yield return new WaitForSeconds(1.0f);
+        // get amount of instantiated fruits
+        //activeFruits = FindObjectsOfType(typeof(prefab)) as Fruit[];
+        //int count = activeFruits.Length;
+       // if (count < maxInstances)
         Instantiate(prefab, position, Quaternion.identity);
         Debug.Log("Fruit Instantiated");
 

e8228c4b67abbbd248d16b991e741ae7d67b3b57
Sven Wehrend <sven@BPN.TU-BERLIN.DE>
2016-08-26 19:08:11 +0200

    gui things

--- Assets/NinjaGame/Editor/NinjaGameEditorWindow.cs
+++ Assets/NinjaGame/Editor/NinjaGameEditorWindow.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System;
+using System.Linq;
 using System.Collections.Generic;
 using Assets.NinjaGame.Scripts;
 
@@ -8,14 +9,15 @@ namespace Assets.NinjaGame.Editor
 {
     public class NinjaGameEditorWindow : EditorWindow
     {
-        public string[] controller = new string[] { "Vive controller / Sword", "Hand(s)", };
+        public string[] controller = new string[] { "Vive controller_ Sword", "Hand(s)", };
         public int index = 0;
         public bool groupEnabled;
-        public List<UnityEngine.Object> resourcePrefabs = new List<UnityEngine.Object>();
+        public UnityEngine.Object[] prefabs;
 
         [MenuItem("NinjaGame/NinjaGame Configuration")]
         static void Init()
         {
+          
             // Get existing open window or if none, make a new one:
             NinjaGameEditorWindow window = (NinjaGameEditorWindow)EditorWindow.GetWindow(typeof(NinjaGameEditorWindow));
             window.Show();
@@ -23,36 +25,31 @@ namespace Assets.NinjaGame.Editor
 
         void OnGUI()
         {
-            PrefabSearch();
+            FindObjectsOfType();
             GUILayout.Label("Ninja Game Configuration", EditorStyles.boldLabel);
             index = EditorGUILayout.Popup(index, controller);
             GUILayout.Label("Available Fruits and Bombs (Prefabs)", EditorStyles.boldLabel);
-            foreach (var prefab in resourcePrefabs)
+            foreach (var prefab in prefabs)
             {
-               EditorGUILayout.Toggle(prefab.name,true);
+                bool prefabchoosen =true;
+                Debug.Log(prefab.name);
+
+                EditorGUILayout.Toggle(prefab.name, prefabchoosen);
             }
 
+            groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
+            EditorGUILayout.EndToggleGroup();
             if (GUILayout.Button("Ok"))
                 DoSomething();
-            groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-    
-            EditorGUILayout.EndToggleGroup();
         }
-        /// <summary>
-        /// ToDo: make to PrefabSearch(Type) or something 
-        /// </summary>
-        void PrefabSearch()
-        {
-            if (resourcePrefabs.Count == 0)
-            {
-                var allResources = Resources.LoadAll("Prefabs", typeof(MovingRigidbodyPhysics));
-                foreach (var res in allResources)
-                {
-                    resourcePrefabs.Add(res);
 
-                }
-            }
+        void FindObjectsOfType()
+        {
+            if (prefabs.Count() == 0)
+                //An array of objects whose class is type or is derived from type.
+                prefabs = Resources.FindObjectsOfTypeAll(typeof(MovingRigidbodyPhysics));
         }
+
 
        void DoSomething()
        {

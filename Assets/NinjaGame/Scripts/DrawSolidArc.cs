18f7e44f796ba14472f173d3844e9b6d8d958020
Sven Wehrend <sven@BPN.TU-BERLIN.DE>
2016-10-10 18:47:52 +0200

    multiple instances

--- Assets/NinjaGame/Scripts/DrawSolidArc.cs
+++
@@ -1,7 +0,0 @@
-﻿using UnityEngine;
-
-[ExecuteInEditMode]
-public class DrawSolidArc : MonoBehaviour
-{
-    public float shieldArea = 5;
-}
--- Assets/NinjaGame/Scripts/SpawnerRangeVisualisation.cs
+++
@@ -1,6 +0,0 @@
-﻿using UnityEngine;
-
-[ExecuteInEditMode]
-public class SpawnerRangeVisualisation : MonoBehaviour {
-    float SpawnerDistance = 5.0f;
-}

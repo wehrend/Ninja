0f8746024e6860fa39dfe23835e4db4f42363f9d
Sven Wehrend <sven@BPN.TU-BERLIN.DE>
2017-01-17 15:44:02 +0100

    baseline

--- Assets/NinjaGame/Scripts/ScoreAndStats.cs
+++ Assets/NinjaGame/Scripts/ScoreAndStats.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Assets.LSL4Unity.Scripts;
 using System.Collections;
 
 namespace Assets.NinjaGame.Scripts
@@ -11,16 +12,21 @@ namespace Assets.NinjaGame.Scripts
         public Text scoresText;
         public Scrollbar healthBar;
         public Scene scene;
+        public float userInitTime=15.00f;
+        public float baselineDuration=180.00f;
+
       //  private NinjaGame.GameInfo scores; 
         [HideInInspector]
         private NinjaGameEventController ninjaGameEvent;
         [HideInInspector]
         public NinjaGameEventArgs eve;
+        private LSLMarkerStream expMarker;
+        private bool initflag, endflag;
         // Use this for initialization
+
         void Start()
         {
             scene = SceneManager.GetActiveScene();
-            // if scene is "Empty_room"...
             //healthBar = GetComponent<Scrollbar>();
             ninjaGameEvent = FindObjectOfType(typeof(NinjaGameEventController)) as NinjaGameEventController;
            
@@ -32,37 +38,84 @@ namespace Assets.NinjaGame.Scripts
             ninjaGameEvent.FruitCollision += new NinjaGameEventHandler(updateFruitCollision);
             ninjaGameEvent.BombCollision += new NinjaGameEventHandler(updateBombCollision);
             ninjaGameEvent.GameOver += new NinjaGameEventHandler(gameOver);
-           
-        }
 
-
-
-        void StuffForBaseline()
-        {
-            if (instructionText)
-                instructionText.text= "Baseline Recording. Please follow the instructions of lab assistant.";
+            expMarker = FindObjectOfType(typeof(LSLMarkerStream)) as LSLMarkerStream;
+            instructionText = GetComponent<Text>();
             scoresText = GetComponent<Text>();
-            if (scoresText)
-                scoresText.text="0 Trials";
         }
+
+
+
+        void InitBaseline()
+        {
+
+            if (!initflag)
+            {
+                Debug.Log("Init baseline...");
+
+                if (scoresText)
+                {
+                    Debug.Log("scores text");
+                    scoresText.color = Color.red;
+                    scoresText.text = "Baseline Recording.\nPlease follow the instructions of lab assistant.";
+                }
+                if (expMarker != null)
+                {
+                    expMarker.Write("begin_baseline_condition");
+                    Debug.Log("begin_baseline_condition");
+                }
+                initflag = true;
+            }
+
+        }
+
+        void EndBaseline() { 
+
+            if (!endflag)
+            {
+
+                Debug.Log("End baseline...");
+                if (expMarker != null)
+                {
+                    expMarker.Write("end_baseline_condition");
+                    Debug.Log("end_baseline_condition");
+                }
+             
+                endflag = true;
+            }
+        }
+
 
         void Update()
         {
-            if (scene.name.Contains("Empty_room"))
-                StuffForBaseline();
+            // aka if no experiment scene loaded 
+            if (NinjaGame.generatedTrials == null)
+            {
+                if (Time.time > userInitTime)
+                {
+                    //Debug.Log(Time.time);
+                    InitBaseline();
+
+                    if ((Time.time - userInitTime) > baselineDuration)
+
+                        EndBaseline();
+
+                }
+            }
             else
             {
+                // aka experiment scene is loaded
                 if (scoresText && NinjaGame.generatedTrials != null)
                     scoresText.text = "Counting Trials :\n" + NinjaGame.generatedTrials.Count;
                 //healthBar.size = (float) NinjaGame.game.health / 1000f;
-            }
 
-            if ((NinjaGame.generatedTrials != null) && (NinjaGame.generatedTrials.Count == 0))
-            {
-                if (instructionText)
+                if (NinjaGame.generatedTrials.Count == 0)
                 {
-                    instructionText.color = Color.blue;
-                    instructionText.text = "Ok. Experiment is finished. Thanks for being part of it";
+                    if (scoresText)
+                    {
+                        scoresText.color = Color.blue;
+                        scoresText.text = "Ok. Experiment is finished. Thanks for being part of it";
+                    }
                 }
             }
         }

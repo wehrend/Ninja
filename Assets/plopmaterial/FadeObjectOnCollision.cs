05b29e5db0e433b57c1b7f83a1f4fc174508f89b
Sven Wehrend <sven@BPN.TU-BERLIN.DE>
2017-06-01 18:32:16 +0200

    bubble effect && audio

--- Assets/plopmaterial/FadeObjectOnCollision.cs
+++ Assets/plopmaterial/FadeObjectOnCollision.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Assets.VREF.Scripts;
+using Valve.VR;
 
 public class FadeObjectOnCollision : MonoBehaviour {
 
@@ -12,42 +13,60 @@ public class FadeObjectOnCollision : MonoBehaviour {
     float stepwidth;
     public int frameSteps=10;
     public float waitTime;
+    public float deltatime;
     [Tooltip("Total Animation Time (ms)")]
-    public int animationTimeDuration=100;
+   // public float animationTimeDuration=0.1f;
     private Vector4 objPos;
     MeshRenderer renderer;
+    Color rendererColor;
+    Color alphaColor;
     Rigidbody body;
     AudioSource audio;
+    private Vector3 statictransformScale;
+
+    int deviceID;
 
     //[RequireComponent(typeof(AudioSource))]
-	// Use this for initialization
-	void Start () {
-        audio =GetComponent<AudioSource>();
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+        deviceID = SteamVR_Controller.GetDeviceIndex(SteamVR_Controller.DeviceRelation.Rightmost);
+        if (deviceID == -1)
+            Debug.LogWarning("No Controller");
+        Debug.Log("Controller:"+deviceID);
         //animation time duration in milliseconds for 100 steps
-        waitTime = animationTimeDuration / (1000f*frameSteps);
+       // waitTime = animationTimeDuration / (1000f*frameSteps);
         stepwidth = 1f / frameSteps;
         renderer = GetComponent<MeshRenderer>();
+        rendererColor = renderer.material.color;
+        alphaColor = Color.white;
+        alphaColor.a = 0f;
         body = GetComponent<Rigidbody>();
+        statictransformScale = transform.localScale * 1.5f;
+
     }
 	
 	// Update is called once per frame
 	void OnCollisionEnter(Collision collisionInfo) {
         fade = 0f;
-        flag = true;
+
         ContactPoint contact;
         Vector3 localPoint;
         // theres only one contact point
         contact = collisionInfo.contacts[0];
-
+        body.velocity = Vector3.zero;
         System.String collidername = contact.otherCollider.name;
         if (collidername.Contains("HandCursor"))
         {
+            flag = true;
             localPoint = contact.thisCollider.transform.localPosition;
             time = Time.realtimeSinceStartup;
             //var collision = new Vector4(contact.point.x, contact.point.y, contact.point.z, time);
             //Debug.Log("DEBUG:"+collision);
             //renderer.material.SetVector("_Collision", collision);
             audio.Play();
+            SteamVR_Controller.Input(deviceID).TriggerHapticPulse(500);
+            contact.thisCollider.enabled = false;
             StartCoroutine(FadeAlphaAndScale());
          } else {
             //Debug.LogWarning("[Destroyed]");
@@ -57,23 +76,40 @@ public class FadeObjectOnCollision : MonoBehaviour {
 
     }
 
+    void Update()
+    {
+        //if (deltatime > 0)
+        //    Debug.LogWarning("deltat:" + deltatime);
+    }
+
     IEnumerator FadeAlphaAndScale()
     {
-        for (fade = 0f; fade <= 1; fade += stepwidth)
-        {
-            transform.localScale += Vector3.one * fade * 0.1f;
-            Color color = renderer.material.color;
-            color.a -= fade;
-            renderer.material.color = color;
-            yield return new WaitForSeconds(waitTime);
-            if (Mathf.Approximately(fade, 1f))
+        //var starttime = Time.realtimeSinceStartup;
+        //for (fade = 0f; fade <= 1; fade += stepwidth)
+        //{
+        //transform.localScale += Vector3.one * fade * 0.1f;
+        //rendererColor.a -= fade;
+        float startTime = Time.time;
+        float animationTimeDuration = 0.1f;
+        while (Time.time < startTime + animationTimeDuration)
+        { 
+          
+            transform.localScale = Vector3.Lerp(transform.localScale, statictransformScale, (Time.time - startTime) / animationTimeDuration);
+            rendererColor = Vector4.Lerp(rendererColor, alphaColor, (Time.time - startTime) /animationTimeDuration);
+            renderer.material.color = rendererColor;
+
+            yield return null;
+        } 
+           
+            if (Mathf.Approximately(rendererColor.a, 0f))
             {
                 //Debug.LogWarning("[Destroyed]");
-                DestroyObject(body.gameObject, 0.5f);
+                DestroyImmediate(body.gameObject);
             }
         }
+        //deltatime = Time.realtimeSinceStartup - starttime;
 
-    }
+    //}
 }
 
 /* old
